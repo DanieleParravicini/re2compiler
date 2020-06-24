1e2690773327d6ba3b795fcbb1e71f63900d1e5c
Daniele Parravicini <daniparra@hotmail.it>
2020-06-24 13:18:02 +0200

    Fix identification of non single entering arrow in split optimizer.

--- optimization.py
+++ optimization.py
@@ -1,6 +1,7 @@
 import ir_lower
 import ir_re2coprocessor
 import itertools
+import json
 
 def eliminate_nops(start_node):
 	#eliminate nop used during construction
@@ -31,11 +32,11 @@ def simplify_jumps(start_node):
 		for node in nodes:
 			for i in range(len(node.children)):
 				if ( isinstance(node.children[i] , ir_lower.Jmp) and 
-				     isinstance(node.children[i].children[0] , ir_lower.Jmp) ):
+					 isinstance(node.children[i].children[0] , ir_lower.Jmp) ):
 					node.replace(node.children[i], ir_lower.Jmp(node.children[i].children[0].children[0]))
 					something_changed = True
 		if (isinstance(start_node , ir_lower.Jmp) and 
-		    isinstance(start_node.children[0] , ir_lower.Jmp)):
+			isinstance(start_node.children[0] , ir_lower.Jmp)):
 		   start_node = start_node.children[0]
 		   something_changed = True
 	
@@ -51,6 +52,18 @@ def reverse(dictionary):
 			revdict.setdefault(v, []).append(k)
 	return revdict
 
+def pretty(d, indent=0):
+	res ='{\n'
+	for key, value in d.items():
+		res+=('\t' * indent + str(key)+'\n')
+		if isinstance(value, dict):
+			res += pretty(value, indent+1)
+		else :
+			res+=('\t' * (indent+1) + str(value)+'\n')
+	res+='}\n'
+	return res
+
+
 def enhance_splits(start_node):
 	# enhance splits
 	split_to_connected_split = {}
@@ -64,12 +77,14 @@ def enhance_splits(start_node):
 		if len(n.children)>0:
 			children[n] = n.children
 
+	#print('childrens', pretty(children))
 	father = reverse(children)
+	#print('fathers', pretty(father))
 	double_fathers = []
 	for child in father:
-		if len(father[child]) > 2:
-			double_fathers.append(child)
-
+		if len(father[child]) >= 2:
+			double_fathers = double_fathers + [child]
+	#print('double fathers' ,double_fathers)
 	#collect splits and directly connected fixed point.
 	for node in split_nodes:
 		if not( node in split_to_connected_split ):
@@ -113,7 +128,7 @@ def enhance_splits(start_node):
 
 		if(split_group == start_node):
 			start_node = new_split_group
-		
+		#print(split_group)
 		for f in father[split_group]:
 			f.replace(split_group,new_split_group )
 
